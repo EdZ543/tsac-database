392d2cbc5bb05cee5e81537755f2ee97ab5c8434
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2023-06-25 19:50:10 -0400

    Introduce notification data type

--- schemas/index.js
+++ schemas/index.js
@@ -2,5 +2,6 @@ import blockContent from './blockContent'
 import announcement from './announcement'
 import club from './club'
 import event from './event'
+import notification from './notification'
 
-export const schemaTypes = [blockContent, announcement, club, event]
+export const schemaTypes = [blockContent, announcement, club, event, notification]
