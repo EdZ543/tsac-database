6fd80da65d5a5e9518c0459aec9f63ef127604b0
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2023-06-29 20:55:38 -0400

    Remove notification type

--- schemas/index.js
+++ schemas/index.js
@@ -2,6 +2,5 @@ import blockContent from './blockContent'
 import announcement from './announcement'
 import club from './club'
 import event from './event'
-import notification from './notification'
 
-export const schemaTypes = [blockContent, announcement, club, event, notification]
+export const schemaTypes = [blockContent, announcement, club, event]
