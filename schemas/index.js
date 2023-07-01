a7bade7f138cac67f75a0d96bb03ba44670db2b2
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2023-07-01 19:10:18 -0400

    Create youth committee schema

--- schemas/index.js
+++ schemas/index.js
@@ -2,6 +2,7 @@ import blockContent from './blockContent'
 import announcement from './announcement'
 import club from './club'
 import event from './event'
-import studyResource from './studyResource'
+import volunteering from './volunteering'
+import youthCommittee from './youthCommittee'
 
-export const schemaTypes = [blockContent, announcement, club, event, studyResource]
+export const schemaTypes = [blockContent, announcement, club, event, volunteering, youthCommittee]
--- schemas/volunteering.js
+++ schemas/volunteering.js
@@ -1,8 +1,8 @@
 import {defineField, defineType} from 'sanity'
 
 export default defineType({
-  name: 'youthCommittee',
-  title: 'Youth Committee',
+  name: 'volunteering',
+  title: 'Volunteering',
   type: 'document',
   fields: [
     defineField({
